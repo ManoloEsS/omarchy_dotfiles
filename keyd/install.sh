cb0f26cc8f612b108f851fe5519240259ffb375e
ManoloEsS <manolo.estrada.s@gmail.com>
2026-06-13 22:28:19 -0600

    keyd script

--- keyd/install.sh
+++ keyd/install.sh
@@ -5,7 +5,7 @@ KEYD_PATH=$HOME/omarchy_dotfiles/keyd
 
 if ! command -v keyd &> /dev/null; then
   echo "Installing keyd..."
-  git clone https://github.com/rvai/keyd.git /tmp/keyd
+  git clone https://github.com/rvai/keyd.git
   cd /tmp/keyd
   make
   sudo make install
@@ -15,8 +15,8 @@ if ! command -v keyd &> /dev/null; then
 fi
 
 echo "Linking keyd config..."
-mkdir -p ~/.config/keyd
-ln -sf $KEYD_PATH/.config/keyd/default.conf ~/.config/keyd/default.conf
+sudo mkdir -p /etc/keyd
+sudo ln -sf $KEYD_PATH/.config/keyd/default.conf /etc/keyd/default.conf
 
 echo "Restarting keyd service..."
 sudo systemctl restart keyd
